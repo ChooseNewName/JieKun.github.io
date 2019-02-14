23383a6cb29b95c76a775177dda73072d60713cd
JieKun <1203664512@qq.com>
2019-02-14 20:36:49 +0800

    简历

--- js/main.js
+++ js/main.js
@@ -1,87 +1,466 @@
 jQuery(document).ready(function($) {
 
-	'use strict';
+    /* ---------------------------------------------------------------------- */
+    /*	------------------------------- Loading ----------------------------- */
+    /* ---------------------------------------------------------------------- */
 
-        $(window).load(function() { // makes sure the whole site is loaded
-            $(".seq-preloader").fadeOut(); // will first fade out the loading animation
-            $(".sequence").delay(500).fadeOut("slow"); // will fade out the white DIV that covers the website.
-        })
-      
-        
-        $(function() {
-  
-        function showSlide(n) {
-            // n is relative position from current slide
-          
-            // unbind event listener to prevent retriggering
-            $body.unbind("mousewheel");
-          
-            // increment slide number by n and keep within boundaries
-            currSlide = Math.min(Math.max(0, currSlide + n), $slide.length-1);
-            
-            var displacment = window.innerWidth*currSlide;
-            // translate slides div across to appropriate slide
-            $slides.css('transform', 'translateX(-' + displacment + 'px)');
-            // delay before rebinding event to prevent retriggering
-            setTimeout(bind, 700);
-            
-            // change active class on link
-            $('nav a.active').removeClass('active');
-            $($('a')[currSlide]).addClass('active');
-            
+    /*Page Preloading*/
+    $(window).load(function() {
+        $('#spinner').fadeOut(200);
+        $('#preloader').delay(200).fadeOut('slow');
+        $('.wrapper').fadeIn(200);
+        $('#custumize-style').fadeIn(200);
+    });
+
+    /* ---------------------------------------------------------------------- */
+    /* ------------------------------- Taps profile ------------------------- */
+    /* ---------------------------------------------------------------------- */
+
+    $('.collapse_tabs').click(function() {
+
+        if ($(this).hasClass('collapsed')) {
+            $(this).find('i.glyphicon').removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
+        } else {
+            $(this).find('i.glyphicon').removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
         }
-      
-        function bind() {
-             $body.bind('false', mouseEvent);
-          }
-      
-        function mouseEvent(e, delta) {
-            // On down scroll, show next slide otherwise show prev slide
-            showSlide(delta >= 0 ? -1 : 1);
-            e.preventDefault();
+
+    });
+
+    /* ---------------------------------------------------------------------- */
+    /* -------------------------- easyResponsiveTabs ------------------------ */
+    /* ---------------------------------------------------------------------- */
+
+    $('#verticalTab').easyResponsiveTabs({
+        type: 'vertical',
+        width: 'auto',
+        fit: true
+    });
+
+    $("h2.resp-accordion").click(function() {
+        $(this).find(".icon_menu").addClass("icon_menu_active");
+        $("h2.resp-accordion").not(this).find(".icon_menu").removeClass("icon_menu_active");
+
+        /*	Scroll To */
+        $('html, body').animate({scrollTop: $('h2.resp-accordion').offset().top - 50}, 600);
+    });
+
+    $(".resp-tabs-list li").click(function() {
+        $(this).find(".icon_menu").addClass("icon_menu_active");
+        $(".resp-tabs-list li").not(this).find(".icon_menu").removeClass("icon_menu_active");
+    });
+
+
+    $(".resp-tabs-list li").hover(function() {
+        $(this).find(".icon_menu").addClass("icon_menu_hover");
+    }, function() {
+        $(this).find(".icon_menu").removeClass("icon_menu_hover");
+    });
+
+    $("h2.resp-accordion").hover(function() {
+        $(this).find(".icon_menu").addClass("icon_menu_hover");
+    }, function() {
+        $(this).find(".icon_menu").removeClass("icon_menu_hover");
+    });
+
+    /* ---------------------------------------------------------------------- */
+    /* --------------------------- Scroll tabs ------------------------------ */
+    /* ---------------------------------------------------------------------- */
+
+    $(".content_2").mCustomScrollbar({
+        theme: "dark-2",
+        contentTouchScroll: true,
+        advanced: {
+            updateOnContentResize: true,
+            updateOnBrowserResize: true,
+            autoScrollOnFocus: false
         }
-        
-        $('nav a, .main-btn a').click(function(e) {
-            // When link clicked, find slide it points to
-            var newslide = parseInt($(this).attr('href')[1]);
-            // find how far it is from current slide
-            var diff = newslide - currSlide - 1;
-            showSlide(diff); // show that slide
-            e.preventDefault();
-        });
-      
-        $(window).resize(function(){
-          // Keep current slide to left of window on resize
-          var displacment = window.innerWidth*currSlide;
-          $slides.css('transform', 'translateX(-'+displacment+'px)');
-        });
-        
-        // cache
-        var $body = $('body');
-        var currSlide = 0;
-        var $slides = $('.slides');
-        var $slide = $('.slide');
-      
-        // give active class to first link
-        $($('nav a')[0]).addClass('active');
-        
-        // add event listener for mousescroll
-        $body.bind('false', mouseEvent);
-    })        
+    });
+
+    /* ---------------------------------------------------------------------- */
+    /* ------------------------- Effect tabs -------------------------------- */
+    /* ---------------------------------------------------------------------- */
+
+    var animation_style = 'bounceIn';
+
+    $('.dropdown-select').change(function() {
+        animation_style = $('.dropdown-select').val();
+    });
 
 
-        $('#form-submit .date').datepicker({
+    $('ul.resp-tabs-list li[class^=tabs-]').click(function() {
+
+        var tab_name = $(this).attr('data-tab-name');
+
+        $('.resp-tabs-container').addClass('animated ' + animation_style);
+        $('.resp-tabs-container').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $('.resp-tabs-container').removeClass('animated ' + animation_style);
         });
 
-
-        $(window).on("scroll", function() {
-            if($(window).scrollTop() > 100) {
-                $(".header").addClass("active");
-            } else {
-                //remove the background property so it comes transparent again (defined in your css)
-               $(".header").removeClass("active");
+        $(".content_2").mCustomScrollbar("destroy");
+        $(".content_2").mCustomScrollbar({
+            theme: "dark-2",
+            contentTouchScroll: true,
+            advanced: {
+                updateOnContentResize: true,
+                updateOnBrowserResize: true,
+                autoScrollOnFocus: false
             }
         });
+
+        if (tab_name == "contact")
+            initialize();
+
+        return false;
+    });
+
+    $("#verticalTab h2.resp-accordion").click(function() {
+        initialize();
+    });
+
+    /* ---------------------------------------------------------------------- */
+    /* ---------------------- redimensionnement ----------------------------- */
+    /* ---------------------------------------------------------------------- */
+
+    function redimensionnement() {
+
+        if (window.matchMedia("(max-width: 800px)").matches) {
+            $(".content_2").mCustomScrollbar("destroy");
+            $(".resp-vtabs .resp-tabs-container").css("height", "100%");
+            $(".content_2").css("height", "100%");
+        } else {
+
+            $(".resp-vtabs .resp-tabs-container").css("height", "580px");
+            $(".content_2").css("height", "580px");
+            $(".content_2").mCustomScrollbar("destroy");
+            $(".content_2").mCustomScrollbar({
+                theme: "dark-2",
+                contentTouchScroll: true,
+                advanced: {
+                    updateOnContentResize: true,
+                    updateOnBrowserResize: true,
+                    autoScrollOnFocus: false
+                }
+            });
+
+        }
+
+    }
+
+    // On lie l'événement resize à la fonction
+    window.addEventListener('load', redimensionnement, false);
+    window.addEventListener('resize', redimensionnement, false);
+
+    $("#verticalTab h2.resp-accordion").click(function() {
+        initialize();
+    });
+
+    /* ---------------------------------------------------------------------- */
+    /* -------------------------- Contact Form ------------------------------ */
+    /* ---------------------------------------------------------------------- */
+
+    // Needed variables
+    var $contactform = $('#contactform'),
+            $success = ' Your message has been sent. Thank you!';
+
+    $contactform.submit(function() {
+        $.ajax({
+            type: "POST",
+            url: "php/contact.php",
+            data: $(this).serialize(),
+            success: function(msg)
+            {
+                var msg_error = msg.split(",");
+                var output_error = '';
+
+                if (msg_error.indexOf('error-message') != -1) {
+                    $("#contact-message").addClass("has-error");
+                    $("#contact-message").removeClass("has-success");
+                    output_error = 'Please enter your message.';
+                } else {
+                    $("#contact-message").addClass("has-success");
+                    $("#contact-message").removeClass("has-error");
+                }
+
+                if (msg_error.indexOf('error-email') != -1) {
+
+                    $("#contact-email").addClass("has-error");
+                    $("#contact-email").removeClass("has-success");
+                    output_error = 'Please enter valid e-mail.';
+                } else {
+                    $("#contact-email").addClass("has-success");
+                    $("#contact-email").removeClass("has-error");
+                }
+
+                if (msg_error.indexOf('error-name') != -1) {
+                    $("#contact-name").addClass("has-error");
+                    $("#contact-name").removeClass("has-success");
+                    output_error = 'Please enter your name.';
+                } else {
+                    $("#contact-name").addClass("has-success");
+                    $("#contact-name").removeClass("has-error");
+                }
+
+
+                if (msg == 'success') {
+
+                    response = '<div class="alert alert-success success-send">' +
+                            '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+                            '<i class="glyphicon glyphicon-ok" style="margin-right: 5px;"></i> ' + $success
+                            + '</div>';
+
+
+                    $(".reset").trigger('click');
+                    $("#contact-name").removeClass("has-success");
+                    $("#contact-email").removeClass("has-success");
+                    $("#contact-message").removeClass("has-success");
+
+                } else {
+
+                    response = '<div class="alert alert-danger error-send">' +
+                            '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+                            '<i class="glyphicon glyphicon-remove" style="margin-right: 5px;"></i> ' + output_error
+                            + '</div>';
+
+                }
+                // Hide any previous response text
+                $(".error-send,.success-send").remove();
+                // Show response message
+                $contactform.prepend(response);
+            }
+        });
+        return false;
+    });
+
+    /* ---------------------------------------------------------------------- */
+    /* ----------------------------- Portfolio ------------------------------ */
+    /* ---------------------------------------------------------------------- */
+
+
+    var filterList = {
+        init: function() {
+
+            // MixItUp plugin
+            // http://mixitup.io
+            $('#portfoliolist').mixitup({
+                targetSelector: '.portfolio',
+                filterSelector: '.filter',
+                effects: ['fade'],
+                easing: 'snap',
+                // call the hover effect
+                onMixEnd: filterList.hoverEffect()
+            });
+
+        },
+        hoverEffect: function() {
+
+            // Simple parallax effect
+            $('#portfoliolist .portfolio').hover(
+                    function() {
+                        $(this).find('.label').stop().animate({bottom: 0}, 200);
+                        $(this).find('img').stop().animate({top: -30}, 500);
+                    },
+                    function() {
+                        $(this).find('.label').stop().animate({bottom: -40}, 200);
+                        $(this).find('img').stop().animate({top: 0}, 300);
+                    }
+            );
+
+        }
+
+    };
+
+    // Run the show!
+    filterList.init();
+
+    /* ---------------------------------------------------------------------- */
+    /* ----------------------------- prettyPhoto ---------------------------- */
+    /* ---------------------------------------------------------------------- */
+
+    $("a[rel^='portfolio']").prettyPhoto({
+        animation_speed: 'fast', /* fast/slow/normal */
+        social_tools: '',
+        theme: 'pp_default',
+        horizontal_padding: 5,
+        deeplinking: false,
+    });
+
+
+
+    /* ---------------------------------------------------------------------- */
+    /* ------------------------------ Google Maps --------------------------- */
+    /* ---------------------------------------------------------------------- */
+
+    var map;
+    function initialize() {
+        map = new GMaps({
+            div: '#map',
+            lat: -37.817917,
+            lng: 144.965065,
+            zoom: 16
+
+        });
+        map.addMarker({
+            lat: -37.81792,
+            lng: 144.96506,
+            title: 'Marker with InfoWindow',
+            icon: 'images/pins-map/map-marker.png',
+            infoWindow: {
+                content: '<p>Melbourne Victoria, 300, Australia</p>'
+            }
+        });
+    }
+
+    /* ---------------------------------------------------------------------- */
+    /* --------------------------------- Blog ------------------------------- */
+    /* ---------------------------------------------------------------------- */
+
+    // More blog
+    $('a.read_m').click(function() {
+        var pagina = $(this).attr('href');
+        var postdetail = pagina + '-page';
+
+        if (pagina.indexOf("#post-") != -1) {
+
+            $('#blog-page').hide();
+
+            $(postdetail).show();
+            $(".tabs-blog").trigger('click');
+        }
+
+        return false;
+
+    });
+
+    // More blog
+    $('a.read_more').click(function() {
+        var pagina = $(this).attr('href');
+        var postdetail = pagina + '-page';
+
+        if (pagina.indexOf("#post-") != -1) {
+
+            $('#blog-page').hide();
+
+            $(postdetail).show();
+            $(".tabs-blog").trigger('click');
+        }
+
+        return false;
+
+    });
+
+    //pagination All
+    $('.content-post a').click(function() {
+        var pagina = $(this).attr('href');
+
+        if (pagina == "#blog") {
+
+            $('.content-post').hide();
+            $('#blog-page').show();
+            $(".tabs-blog").trigger('click');
+
+        }
+
+        return false;
+
+    });
+
+    //pagination blog
+    $('.content-post #pagination').click(function() {
+
+
+        var pagina = $(this).attr('href');
+        var postdetail = pagina + '-page';
+
+        if (pagina.indexOf("#post-") != -1) {
+
+            $('#blog-page').hide();
+            $('.content-post').hide();
+
+            $(postdetail).show();
+            $(".tabs-blog").trigger('click');
+        }
+
+        return false;
+
+    });
+
+
+    /* ---------------------------------------------------------------------- */
+    /* ---------------------------- icon menu ------------------------------- */
+    /* ---------------------------------------------------------------------- */
+
+    $(".resp-tabs-container h2.resp-accordion").each(function(){
+			 
+			if($(this).hasClass('resp-tab-active')){
+				$(this).append("<i class='glyphicon glyphicon-chevron-up arrow-tabs'></i>");
+			}else {
+				$(this).append("<i class='glyphicon glyphicon-chevron-down arrow-tabs'></i>");
+			}
+	  });
+	  
+	   $(".resp-tabs-container h2.resp-accordion").click(function(){
+			if($(this).hasClass('resp-tab-active')){
+				$(this).find("i.arrow-tabs").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
+			}
+			
+			$(".resp-tabs-container h2.resp-accordion").each(function(){
+		 
+				if(!$(this).hasClass('resp-tab-active')){
+					$(this).find("i.arrow-tabs").removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
+				}
+		  });
+	  
+			
+	  });
+
+
+    /* ---------------------------------------------------------------------- */
+    /* -------------------------------- skillbar ---------------------------- */
+    /* ---------------------------------------------------------------------- */
+
+    $('.tabs-resume').click(function() {
+
+        $('.skillbar').each(function() {
+            $(this).find('.skillbar-bar').width(0);
+        });
+
+        $('.skillbar').each(function() {
+            $(this).find('.skillbar-bar').animate({
+                width: $(this).attr('data-percent')
+            }, 2000);
+        });
+
+    });
+
+    $('#resume').prev('h2.resp-accordion').click(function() {
+
+        $('.skillbar').each(function() {
+            $(this).find('.skillbar-bar').width(0);
+        });
+
+        $('.skillbar').each(function() {
+            $(this).find('.skillbar-bar').animate({
+                width: $(this).attr('data-percent')
+            }, 2000);
+        });
+    });
+	
+		
+	//Change for demo page
+    $('input:radio[name=page_builder]').on('change', function() {
+		
+		$('input:radio[name=page_builder]').each(function () {
+
+			var $this = $(this);
+	
+			if ($(this).prop('checked')) {
+				window.location.replace($this.val());
+			}
+		});
+		
+        return false;
+    });
+
 
 
 });
